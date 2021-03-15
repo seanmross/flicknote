514ce0da27a0240349a98e101fa49be28c2ad029
Sean Ross <seanross917@gmail.com>
2021-03-15 17:14:10 -0400

    update env variables

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyB9qZg-UIveLfL6R7OcHkcjeEOPAL8BOyM';
-
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
-    key: API_KEY,
+    key: process.env.REACT_APP_GOOGLE_API_KEY,
     part: 'snippet',
     chart: 'mostPopular',
     maxResults: 5
